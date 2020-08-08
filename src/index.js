5c705c60c99cded3fb6684e615b2f181bb1acbe5
Cook <scott.madison.cook@gmail.com>
2020-08-08 14:36:38 -0700

    Dumbass

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {  createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 
 
 const theme = createMuiTheme({
   palette: {
-    main: {
-      primary: '#'
+    primary: {
+      main: "#0069d2"
     }
   }
 })
